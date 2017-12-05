eda842813e4ab3aac8b1d0525f1fb064902c67cd
박혜림 <asap789456@daum.net>
2017-12-05 15:09:41 +0900

    Initial commit

---
+++ app/src/main/java/com/example/parkhaerim/Fragment3.java
@@ -0,0 +1,58 @@
+package com.example.parkhaerim;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.RelativeLayout;
+import android.widget.Toast;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.Toast;
+import com.example.parkhaerim.TextFileManager;
+
+
+
+public class Fragment3 extends Fragment {
+
+    public Fragment3()
+    {
+        // required
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container,
+
+                             @Nullable Bundle savedInstanceState) {
+        RelativeLayout layout = (RelativeLayout)inflater.inflate(R.layout.fragment_fragment3,
+
+                container, false);
+        return layout;
+    }
+
+
+}
+
